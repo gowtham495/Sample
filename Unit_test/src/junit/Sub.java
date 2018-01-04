acbfdb5f6c6afbd50f2e667aded14209721ef295
670334 <670334@PC223504.cts.com>
2018-01-04 11:14:48 +0530

    Unit test

---
+++ Unit_test/src/junit/Sub.java
@@ -0,0 +1,31 @@
+package junit;
+
+public class Sub {
+ int a,b;
+ String c,d;
+ 
+	public Sub(int a, int b) {
+	super();
+	this.a = a;
+	this.b = b;
+}
+
+	public Sub(String c, String d) {
+		super();
+		this.c = c;
+		this.d = d;
+	}
+public int subtraction() {
+	return a-b;
+}
+public String combine() {
+	return c+d;
+}
+	public static void main(String[] args) {
+		Sub s1 = new Sub(5,3);
+		Sub s2 = new Sub("praveen","kumar");
+System.out.println("Difference:"+s1.subtraction());
+System.out.println("Combine:"+s2.combine());
+	}
+
+}
