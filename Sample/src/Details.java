ff080c25e7efd2958f2751b73c1964e535489f3a
670334 <670334@PC223504.cts.com>
2018-01-04 11:49:51 +0530

    01

---
+++ Sample/src/Details.java
@@ -0,0 +1,18 @@
+
+public class Details {
+	String name;
+	int rollno;
+	public Details(String name, int rollno) {
+		super();
+		this.name = name;
+		this.rollno = rollno;
+	}
+	//aaa
+	public void display()
+	{
+		System.out.print(name+"-"+rollno);
+		System.out.println("Hi");
+	}
+	
+	
+}
